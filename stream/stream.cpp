5d0d2f41437302a6acefedb6bc1b4b53e1dadb21
wang wei <wangwei@smartee.cn>
2022-11-22 17:54:58 +0800

    add stream case

---
+++ stream/stream.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
+void testWriteFileDirNotExist()
+{
+    //如果目录不存在，不会自动创建目录
+    ostringstream oss;
+    oss << "./test/output.log";
+    std::ofstream fout(oss.str(), std::ios::binary);
+    fout.write("hello stream", strlen("hello stream"));
+    fout.close();
+}
+
+int main()
+{
+    testWriteFileDirNotExist();
+    return 0;
+}
