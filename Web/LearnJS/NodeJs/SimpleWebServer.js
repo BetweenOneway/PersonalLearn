716296da9e899ee2637be53ece101edabcb9cab9
wang wei <wangwei@smartee.cn>
2023-09-12 18:21:14 +0800

    update some test code

--- Web/LearnJS/NodeJs/SimpleWebServer.js
+++ Web/LearnJS/NodeJs/SimpleWebServer.js
@@ -5,4 +5,13 @@ app.on('request',function(req,res){
     console.log(req.url);
     console.log(req.method);
     //console.log(req);
+
+    res.writeHead(200,{
+        //"Access-Control-Allow-Origin":"http://127.0.0.1:5500",
+        "Access-Control-Allow-Origin":"*",//可伪装成任意网址
+        //内容类型      普通文本
+        "Content-Type":"text/plain;charset=utf-8"//避免乱码
+      })
+      res.write("Hello world!");
+      res.end();
 });
