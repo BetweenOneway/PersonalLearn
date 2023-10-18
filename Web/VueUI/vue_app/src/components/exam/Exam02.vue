89118bc9fa0c2eb104d64872193c51bd75014c99
One Way <oneway2021@163.com>
2023-10-18 23:49:28 +0800

    add some test cases

--- Web/VueUI/vue_app/src/components/exam/Exam02.vue
+++ Web/VueUI/vue_app/src/components/exam/Exam02.vue
@@ -7,16 +7,57 @@
         <div>
             <mt-field label="用户名" placeholder="请输入用户名" :attr="{maxlength:10}" v-model="uname"></mt-field>
             <mt-field label="密码" placeholder="请输入密码" type="password" v-model="upwd"></mt-field>
-            <mt-button size="large">用户注册</mt-button>
+            <mt-button size="large" @click="reg">用户注册</mt-button>
+        </div>
+        <br /><hr/>
+        <div>
+            <mt-switch v-model="val" @change="switch1">开关选项</mt-switch>
+            <mt-radio title="单选列表" :options="['a','b','c']" v-model="opt"></mt-radio>
+            <mt-radio title="单选列表1" :options="options" v-model="opt1"></mt-radio>
+            <mt-button @click="GetVal">获取当前选项值</mt-button>
         </div>
     </div>
 </template>
 <script>
     export default{
         data(){
-            return {uname:"",upwd:""}
+            return {
+                uname:"",
+                upwd:"",
+                val:true,
+                opt:"",
+                opt1:"",
+                options:[
+                    {label:"去年",value:"24"},
+                    {label:"今年",value:"23"},
+                    {label:"明年",value:"22"}
+                ]
+            }
         },
         methods:{
+            GetVal(){
+                this.$toast({message:this.opt1});
+            },
+            switch1()
+            {
+                console.log(this.val);
+            },
+            reg()
+            {
+                //验证用户名 3~12位字母数字
+                var regu = /^\w{3,12}$/i;
+                //验证年龄2位数字
+                var rega = /^\d{2}$/i;
+                var u = this.uname;
+                var p = this.upwd;
+                if(!regu.test(u))
+                {
+                    this.$messagebox({message:"用户名格式不正确"});
+                    return;
+                }
+                //if(!rega.test())
+
+            },
             handle3(){
                 this.$messagebox.prompt("请输入年龄：")
                 .then(value=>{
