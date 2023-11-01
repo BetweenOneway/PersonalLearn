ceb690eb9b6c7964050718caa5ab9f0767d673a6
One Way <oneway2021@163.com>
2023-11-02 00:33:09 +0800

    implement frontpage addcart

--- Web/VueUI/vue_app/src/components/xz/Product.vue
+++ Web/VueUI/vue_app/src/components/xz/Product.vue
@@ -4,7 +4,7 @@
             <img :src="'http://127.0.0.1:8080/'+item.img_url" alt="">
             <h5>{{item.lname}}</h5>
             <div class="info">{{item.price}}</div>
-            <mt-button>加入购物车</mt-button>
+            <mt-button @click="addcart" :data-lid="item.lid" :data-price="item.price" :data-lname="item.lname">加入购物车</mt-button>
         </div>
         <mt-button size="large" @click="loadMore">加载更多</mt-button>
         <mt-button size="large">查看购物车</mt-button>
@@ -31,6 +31,30 @@
                     var rows = this.list.concat(res.data.data);
                     this.list = rows;
                 })
+            },
+            addcart(event){
+                var lid = event.target.dataset.lid;
+                var lname = event.target.dataset.lname;
+                var price = event.target.dataset.price;
+                console.log(lid+"|"+lname+"|"+price);
+                var url="addcart";
+                var obj={lid:lid,lname:lname,price:price};
+                this.axios.get(url,{params:obj}).then(res=>{
+                    console.log(res);
+                    if(res.data.code == -1)
+                    {
+                        this.$messagebox("消息","请登录");
+                        this.$router.push("/Login");
+                    }
+                    else if(res.data.code == -2)
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
+                });
             }
         },
         props:{},
