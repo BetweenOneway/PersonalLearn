048dc3e9760059b7312c64f24d3777b728437953
One Way <oneway2021@163.com>
2023-11-20 23:14:31 +0800

    draw background

--- Web/game/js/main.js
+++ Web/game/js/main.js
@@ -1,7 +1,7 @@
 
 //两个画布的全局对象
-var can1;
-var can2;
+var frontCanvas;
+var backgroundCanvas;
 //两个画笔的全局对象
 var ctx1;
 var ctx2;
@@ -14,23 +14,29 @@ var lastTime;
 //时间差
 var deltaTime;
 
+//背景图片对象
+var bgPic;
+
 function game(){
     init();
     gameloop();
 }
 
 function init(){
-    can1 = document.getElementById("canvas1");
-    can2 = document.getElementById("canvas2");
+    frontCanvas = document.getElementById("canvas1");
+    backgroundCanvas = document.getElementById("canvas2");
 
-    ctx1 = can1.getContext("2d");
-    ctx2 = can2.getContext("2d");
+    ctx1 = frontCanvas.getContext("2d");
+    ctx2 = backgroundCanvas.getContext("2d");
 
-    canWidth = can1.width;
-    canHeight = can1.height;
+    canWidth = frontCanvas.width;
+    canHeight = frontCanvas.height;
 
     lastTime = Date.now();
     deltaTime = 0;
+
+    bgPic = new Image();
+    bgPic.src = "./res/background.jpg"
 }
 
 function gameloop(){
@@ -38,7 +44,8 @@ function gameloop(){
     var now = Date.now();
     deltaTime = now - lastTime;
     lastTime = now;
-    console.log(deltaTime);
+    
+    ctx2.drawImage(bgPic,0,0);
 }
 
 document.body.onload = game;
