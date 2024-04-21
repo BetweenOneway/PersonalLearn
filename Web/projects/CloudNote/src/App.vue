67e5bb4de6197542c0a79ee2a08a7176b20704b4
One Way <oneway2021@163.com>
2024-04-22 00:44:45 +0800

    add new project CloudNote

---
+++ Web/projects/CloudNote/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <n-button>naive-ui</n-button>
+    <n-icon size="30" :component="AcUnitRound"></n-icon>
+  </template>
+  
+  <script setup>
+    import {AcUnitRound} from '@vicons/material'
+  </script>
