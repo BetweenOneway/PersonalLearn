2e4a8d791c819cf6b2920ed537aebc07d3d16afc
wangwei <wangwei@smartee.local>
2024-04-26 11:33:07 +0800

    增加顶部标题栏右侧状态功能区布局

--- Web/projects/CloudNote/src/App.vue
+++ Web/projects/CloudNote/src/App.vue
@@ -1,8 +1,22 @@
 <template>
-    <n-button>naive-ui</n-button>
-    <n-icon size="30" :component="AcUnitRound"></n-icon>
-  </template>
+    <n-layout position="absolute">
+        <!--应用头-->
+        <n-layout-header bordered style="height: 64px;padding:0 20px;">
+            <MainTopToolbar />
+        </n-layout-header>
+        <n-layout position="absolute" has-sider style="top:64px">
+            <!--左侧应用栏-->
+            <n-layout-sider bordered width="64px" content-style="padding: 24px 0; text-align:center;">
+                海淀桥
+            </n-layout-sider>
+            <!--主页面-->
+            <n-layout-content content-style="padding: 24px;">
+                平山道
+            </n-layout-content>
+        </n-layout>
+    </n-layout>
+</template>
   
   <script setup>
-    import {AcUnitRound} from '@vicons/material'
+    import MainTopToolbar from "./components/toolbar/MainTopTooolbar.vue"
   </script>
---
+++ Web/projects/CloudNote/src/components/toolbar/MainTopTooolbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <n-space justify="space-between" align="center" style="height: 100%;">
+        <n-text>之间笔记</n-text>
+        <n-space>
+            <!--头像-->
+            <n-avatar round size="small" style="position:relative;top:3px" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"/>
+            <!--分割线-->
+            <n-divider vertical style="position:relative;top:5px;"/>
+            <!--消息-->
+            <n-badge dot processing type="success" :offset="[-8,4]">
+                <n-button circle tertiary>
+                    <n-icon size="18" :component="NotificationsNoneOutlined" />
+                </n-button>
+            </n-badge>
+
+            <!--主题按钮-->
+            <n-button circle tertiary>
+                <n-icon size="18" :component="DarkModeRound" />
+            </n-button>
+            <n-button tertiary type="primary">登录</n-button>
+        </n-space>
+        
+    </n-space>
+</template>
+<script setup>
+    import {DarkModeRound, NotificationsNoneOutlined} from "@vicons/material"
+</script>
