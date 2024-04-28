8ec368b397139a6c3427eab19236c41d087f684e
wangwei <wangwei@smartee.local>
2024-04-28 13:19:04 +0800

    优化主题切换，增加持久化存储
    增加登录模态框以及信息验证

--- Web/projects/CloudNote/src/main.js
+++ Web/projects/CloudNote/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
@@ -9,9 +10,12 @@ import 'vfonts/Lato.css'
 // 等宽字体
 import 'vfonts/FiraCode.css'
 
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
--- Web/projects/CloudNote/src/stores/themeStore.js
+++ Web/projects/CloudNote/src/stores/themeStore.js
@@ -3,29 +3,38 @@ import { ref,computed } from 'vue'
 import { darkTheme } from "naive-ui"
 import { LightbulbOutlined,DarkModeRound } from '@vicons/material'
 
-export const useThemeStore = defineStore("theme",()=>{
-    //是否是暗系主题
-    const isDarkTheme = ref(false);
-    const theme = computed(()=>{
-        if(isDarkTheme.value)
-        {
-            return {
-                name:darkTheme,
-                icon:LightbulbOutlined
+export const useThemeStore = defineStore(
+    "theme",
+    ()=>{
+        //是否是暗系主题
+        const isDarkTheme = ref(false);
+        const theme = computed(()=>{
+            if(isDarkTheme.value)
+            {
+                return {
+                    name:darkTheme,
+                    icon:LightbulbOutlined
+                }
             }
-        }
-        else{
-            return {
-                name:null,
-                icon:DarkModeRound
+            else{
+                return {
+                    name:null,
+                    icon:DarkModeRound
+                }
             }
+        });
+
+        //更改主题
+        const changeTheme = dark=>{
+            isDarkTheme.value = dark;
         }
-    });
 
-    //更改主题
-    const changeTheme = dark=>{
-        isDarkTheme.value = dark;
-    }
-
-    return {isDarkTheme,theme,changeTheme}
-})
+        return {isDarkTheme,theme,changeTheme}
+    },
+    {
+        persist: {
+            storage:localStorage,//本地存储
+            paths:["isDarkTheme"] //将isDarkTheme持久化保存
+        }
+    },
+)
