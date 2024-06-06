ff8a777ff81ca93e2fdcb6e99228817596c4e9d5
wangwei <wangwei@smartee.local>
2024-06-06 18:15:06 +0800

    add new file learn async & promise

--- Web/projects/CloudNoteServer/routes/memo.js
+++ Web/projects/CloudNoteServer/routes/memo.js
@@ -10,7 +10,7 @@ let statusCode = require("../statusCode")
 var router=express.Router();
 
 //获取用户所有正常的便签
-router.get("/getUserMemoList",(req,res)=>{
+router.get("/getUserMemoList",async (req,res)=>{
     var output={
         success:true,
         status:'',
@@ -24,46 +24,18 @@ router.get("/getUserMemoList",(req,res)=>{
     let userTokenRedisValue={};
     //验证用户是否登录
     try{
-        redisOper.RedisGet(userToken).then((val)=>{
-            userTokenRedisValue = val
-            console.log("retrived redis value:")
-            console.log(userTokenRedisValue)
-            if(userTokenRedisValue == null)
-            {
-                //用户未登录
-                output.success = statusCode.SERVICE_STATUS.NOT_LOGIN.success
-                output.status = statusCode.SERVICE_STATUS.NOT_LOGIN.status
-                output.description = statusCode.SERVICE_STATUS.NOT_LOGIN.description
-                res.send(output)
-                return
-            }
-            
-            userInfo = JSON.parse(userTokenRedisValue)
-            console.log("parsed userinfo")
-            console.log(userInfo)
-            //查询当前用户的所有正常的便签
-            //置顶在前 未完成在前 时间越近的越在前
-            var sql = `SELECT id,title,top,tags,update_time,finished from z_thing WHERE u_id = ? AND status= ? ORDER BY top DESC,finished ASC,update_time DESC;`
-            pool.getConnection(function(error,connection){
-                connection.query(sql, [userInfo.id,status], function (error, results, fields) {
-                    if (error) {
-                        return connection.rollback(function() {
-                            output.success = statusCode.DB_STATUS.SELECT_FAIL.success
-                            output.status = statusCode.DB_STATUS.SELECT_FAIL.status
-                            output.description = statusCode.DB_STATUS.SELECT_FAIL.description
-                            res.send(output);
-                            return
-                        });
-                    }
-                    console.log(results)
-                    output.success = statusCode.SERVICE_STATUS.GET_MEMO_SUCCESS.success
-                    output.status = statusCode.SERVICE_STATUS.GET_MEMO_SUCCESS.status
-                    output.description = statusCode.SERVICE_STATUS.GET_MEMO_SUCCESS.description
-                    res.send(output);
-                    return
-                })
-            })
-        })
+        await redisOper.RedisGet(userToken,userTokenRedisValue)
+        console.log("retrived redis value:")
+        console.log(userTokenRedisValue)
+        if(userTokenRedisValue == null)
+        {
+            //用户未登录
+            output.success = statusCode.SERVICE_STATUS.NOT_LOGIN.success
+            output.status = statusCode.SERVICE_STATUS.NOT_LOGIN.status
+            output.description = statusCode.SERVICE_STATUS.NOT_LOGIN.description
+            res.send(output)
+            return
+        }
     }
     catch(error)
     {
@@ -75,6 +47,32 @@ router.get("/getUserMemoList",(req,res)=>{
         res.send(output)
         return
     }
+
+    userInfo = JSON.parse(userTokenRedisValue)
+    console.log("parsed userinfo")
+    console.log(userInfo)
+    //查询当前用户的所有正常的便签
+    //置顶在前 未完成在前 时间越近的越在前
+    var sql = `SELECT id,title,top,tags,update_time,finished from z_thing WHERE u_id = ? AND status= ? ORDER BY top DESC,finished ASC,update_time DESC;`
+    pool.getConnection(function(error,connection){
+        connection.query(sql, [userInfo.id,status], function (error, results, fields) {
+            if (error) {
+                return connection.rollback(function() {
+                    output.success = statusCode.DB_STATUS.SELECT_FAIL.success
+                    output.status = statusCode.DB_STATUS.SELECT_FAIL.status
+                    output.description = statusCode.DB_STATUS.SELECT_FAIL.description
+                    res.send(output);
+                    return
+                });
+            }
+            console.log(results)
+            output.success = statusCode.SERVICE_STATUS.GET_MEMO_SUCCESS.success
+            output.status = statusCode.SERVICE_STATUS.GET_MEMO_SUCCESS.status
+            output.description = statusCode.SERVICE_STATUS.GET_MEMO_SUCCESS.description
+            res.send(output);
+            return
+        })
+    })
     
 })
 module.exports=router;
--- Web/projects/CloudNoteServer/routes/redisOper.js
+++ Web/projects/CloudNoteServer/routes/redisOper.js
@@ -1,33 +1,29 @@
 const redis = require('redis');
 const redisClient = redis.createClient('6379', '127.0.0.1')
 
-function redisGet(key) {
-    // (async function(){
-    //     await redisClient.connect()
-    //     redisClient.on('error', err => {
-    //         console.error(err) // 打印监听到的错误信息
-    //     })
-    //     redisClient.get(key, (err, result) => {
-    //       if (err) {
-    //         reject(err);
-    //       } else {
-    //         resolve(result);
-    //       }
-    //     });
-    // })();
-    return new Promise(async(resolve, reject) =>  {
-        await redisClient.connect()
-        redisClient.on('error', err => {
-            console.error(err) // 打印监听到的错误信息
-        })
-        redisClient.get(key, (err, result) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(result);
+async function redisGet(key,value) {
+    console.log("redis get:"+key)
+    await redisClient.connect()
+    redisClient.on('error', err => {
+        console.error(err) // 打印监听到的错误信息
+    })
+    console.log("redis connected")
+    return new Promise((resolve, reject) => {
+        console.log("redis start get")
+        redisClient.get(key).then(
+            (err, result) => {
+                if (err) {
+                    console.log("redis get error")
+                    throw err
+                    reject(err)
+                } else {
+                    value = result
+                    console.log("get %o",value)
+                    resolve()
+                }
             }
-        });
-    });
+        );
+    })
 }
 
 module.exports.RedisGet = redisGet
