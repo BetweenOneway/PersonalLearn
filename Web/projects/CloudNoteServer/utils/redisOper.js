f72584e875ce14e45085f87f1e06ab122b103f8b
One Way <oneway2021@163.com>
2024-06-19 23:08:30 +0800

    修复本地IP解析导致的redis连接错误 统一使用IPV4连接

--- Web/projects/CloudNoteServer/utils/redisOper.js
+++ Web/projects/CloudNoteServer/utils/redisOper.js
@@ -1,5 +1,7 @@
 const redis = require('redis');
-const redisClient = redis.createClient({port:'6379', host:'127.0.0.1'})
+const redisClient = redis.createClient({port:'6379', host:'127.0.0.1',socket: {
+    family: 4
+  }})
 
 async function redisConnect()
 {
