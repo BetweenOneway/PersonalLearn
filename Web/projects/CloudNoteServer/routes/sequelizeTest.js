f1aa7be68d9c67ca3f4e779818b5815343712d33
wangwei <wangwei@smartee.local>
2024-07-08 13:53:05 +0800

    sequelizeTest新增事务处理测试用例

--- Web/projects/CloudNoteServer/routes/sequelizeTest.js
+++ Web/projects/CloudNoteServer/routes/sequelizeTest.js
@@ -107,4 +107,61 @@ router.get('/updateUser',async (req,res)=>{
     res.send("User，修改成功")
 })
 
+//非托管事务
+router.get('/unmanagedTransaction',async (req,res)=>{
+    console.log("Add user with transaction")
+    const email = req.query.email;
+    const nickName = req.query.nickname;
+    const passWord = "e10adc3949ba59abbe56e057f20f883e"
+    const level = 0;
+    const curDate = new Date;
+    const curDateFormated = curDate.toISOString().slice(0, 19).replace('T', ' ')
+    const status = 1
+
+    // 首先, 我们从你的连接开始一个事务并将其保存到一个变量中
+    const t = await sqldb.sequelize.transaction();
+
+    try {
+        // 然后,我们进行一些调用以将此事务作为参数传递:
+        const user = await sqldb.User.create(
+            {
+                email:email,
+                password:passWord,
+                nickname:nickName,
+                level:level,
+                time:curDateFormated,
+                status:status
+            },
+            {
+                //指定新增哪些字段
+                fields:['email','password','nickname','level','time','status'],
+                transaction: t
+            }
+        );
+
+        await sqldb.UserLog.create(
+            {
+                u_id: user.id,
+                desc: '新增用户',
+                time:curDateFormated,
+                event:'用户注册'
+            }, 
+            { 
+                transaction: t 
+            }
+        );
+
+        // 如果执行到此行,且没有引发任何错误.
+        // 我们提交事务.
+        await t.commit();
+
+    } catch (error) {
+        console.log("新增用户出错：",error)
+        // 如果执行到达此行,则抛出错误.
+        // 我们回滚事务.
+        await t.rollback();
+
+    }
+    res.send("transaction complete")
+})
 module.exports=router;
