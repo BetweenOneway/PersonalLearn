f7890762e0cd6501fbfe0ba231e487e7381df6ed
wangwei <wangwei@smartee.local>
2024-07-24 11:22:01 +0800

    增加日志处理utility

---
+++ Web/projects/CloudNoteServer/utils/log.js
@@ -0,0 +1,27 @@
+const { Op } = require("sequelize");
+let statusCode = require("./statusCode");
+//数据库
+var sqldb = require('../sqldb');
+
+async function AddEventLog(userId,noteId,event,transaction)
+{
+    try {
+        const newAddedLog = await sqldb.NoteMemoLog.create(
+            {
+                time: date,
+                event: event.code,
+                desc:event.desc,
+                u_id:userId,
+                t_id:noteId
+            }, 
+            { 
+                transaction: transaction 
+            }
+        )
+    } catch (error) {
+        console.log("Add event log failed:",event.desc)
+        return false;
+    }
+}
+
+module.exports.AddEventLog = AddEventLog
