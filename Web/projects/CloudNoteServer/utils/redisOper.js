29065c6928279b4cbd83e31c7e482b83c74c0c5d
wangwei <wangwei@smartee.local>
2024-08-28 11:05:45 +0800

    redis增加连接异常处理 修复频繁连接导致的报错问题

--- Web/projects/CloudNoteServer/utils/redisOper.js
+++ Web/projects/CloudNoteServer/utils/redisOper.js
@@ -3,11 +3,36 @@ const redisClient = redis.createClient({port:'6379', host:'127.0.0.1',socket: {
     family: 4
   }})
 
+// 将Redis连接实例作为全局变量
+//global.redisClient = redisClient;
+
+// 监听beforeExit事件
+process.on('beforeExit', () => {
+    console.log("before exit");
+    // 关闭Redis连接
+    redisClient.quit();
+  }
+);
+
+// 错误处理
+redisClient.on('error', (error) => {
+    // 输出错误信息
+    console.error('Redis Error:', error);
+
+    // 自动重连
+    redisClient.quit();
+    redisClient.connect();
+});
+
 async function redisConnect()
 {
     try
     {
-        await redisClient.connect()
+        // Check if the socket is already opened
+        if (!redisClient.isOpen) {
+            await redisClient.connect()
+        }
+        
         redisClient.on('error',err=>{
             console.log("redis connect error")
             console.log(err)
