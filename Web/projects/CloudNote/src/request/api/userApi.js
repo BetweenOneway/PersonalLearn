1ba9a15290a5df5d2737c761f33675ee546a7ef4
One Way <oneway2021@163.com>
2024-09-09 01:03:09 +0800

    配置登陆请求参数

---
+++ Web/projects/CloudNote/src/request/api/userApi.js
@@ -0,0 +1,12 @@
+const userApi = {
+    loginByMailPassword:{
+        name:'邮箱密码登陆',
+        url:'/user/login',
+        method:'POST',
+        userPower:false,
+        dataParam:true,
+        successMessage:true
+    }
+}
+
+export default userApi
