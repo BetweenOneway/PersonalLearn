7d1079dd4bf2d5e2d1351a4368888d6e79f22969
One Way <oneway2021@163.com>
2024-09-09 01:02:36 +0800

    实现请求拦截器和响应拦截器

--- Web/projects/CloudNote/src/request/index.js
+++ Web/projects/CloudNote/src/request/index.js
@@ -1,10 +1,66 @@
 import axios from 'axios'
 import qs from 'qs'
+import { createDiscreteApi } from 'naive-ui'
+import { getUserToken, loginInvalid } from '../Utils/userLogin'
 
-const request = config =>{
+//脱离上下文的消息和加载条对象
+const { message, loadingBar } = createDiscreteApi(
+    ['message', 'loadingBar']
+)
+
+/*
+请求时处理
+*/
+const request = async config =>{
     //加载条开始
+    loadingBar.start();
+    //判断是否需要请求头中的userToken
+    if(config.userPower) config.headers.userToken = await getUserToken();
+    //是否需要将请求体中的参数转换成URL参数
+    if(config.dataParam)
+    {
+        config.transformRequest=[(data)=>qs.stringify(data)]
+    }
+    return config;
+}
+
+/*
+请求出错时处理
+*/
+const requestError = error =>{
+    //加载条异常结束
+    loadingBar.error();
+    //采用消息显示失败的原因
+    message.error("发送"+error.config.name+"请求失败");
+    //返回失败的原因
+    //return Promise.reject(error);
+}
+
+/*
+响应处理
+*/
+const requestResponse = response =>{
     //
-    //是否需要将请求体中的参数转成URL的参数
+    const responseData = response.data;
+    //
+    if(!responseData.success)
+    {
+        loadingBar.error();
+        message.error(responseData.description)
+        //判断是否登陆失败
+        if(responseData.status ==='SERVICE_008')
+        {
+            loginInvalid(true)
+        }
+        return null
+    }
+    loadingBar.finish();
+    //判断是否需要弹出成功消息 如登陆
+    if(response.config.successMessage)
+    {
+        message.success(response.config.name + "成功");
+    }
+    return responseData;
 }
 
 //自定义请求对象
@@ -14,20 +70,16 @@ const noteServerRequest = axios.create({
 
 //添加请求拦截器
 noteServerRequest.interceptors.request.use(
-    config=>{
-
-    },
-    error=>{
-
-    }
+    config=>request(config),
+    error=>requestError(error)
 );
 
 noteServerRequest.interceptors.response.use(
     response=>{
-
+        requestResponse(response);
     },
     error=>{
-
+        requestError(error);
     }
 );
 
