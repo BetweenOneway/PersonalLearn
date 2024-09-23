4714b46681202d3b6b680ed07129922f93bede29
wangwei <wangwei@smartee.local>
2024-09-23 11:45:04 +0800

    学习 测试 拦截器

--- Web/NodeJs/LearnExpress/app.js
+++ Web/NodeJs/LearnExpress/app.js
@@ -17,5 +17,21 @@ app.use(bodyParser.urlencoded({extended:false}));
 //托管静态资源到public目录下
 //app.use(express.static('public'));
 
+//拦截要放在所有路由之前
+app.all('*', (req, res, next) => {
+    console.log('Accessing interceptor ...')
+    req.first_name = "wang"
+    req.last_name = "between"
+    next() // pass control to the next handler
+  }
+)
+
+//全局异常处理(无效)
+// app.use(function(err, req, res, next) {
+//     console.error("err happened:",err);
+//     res.status(500).send('Something broke!');
+//   }
+// );
+
 /*使用路由器来管理路由*/
 app.use("/test",test);
--- Web/NodeJs/LearnExpress/routes/test.js
+++ Web/NodeJs/LearnExpress/routes/test.js
@@ -4,9 +4,12 @@ var router=express.Router();
 
 router.get("/testMultiCallback",function (req, res, next) {
     console.log('the response will be sent by the next function ...')
+    console.log("req.first_name",req.first_name);
     next()//必须调用，否则不会调用下一个回调
   }, function (req, res) {
     console.log('Hello from B!');
+    console.log("req.last_name",req.last_name);
+    
     res.send('Hello from B!')
   }
 )
