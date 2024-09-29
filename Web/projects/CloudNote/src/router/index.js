991602c6b8c71a7369b0227efd141d5437b053a5
wangwei <wangwei@smartee.local>
2024-09-29 11:28:35 +0800

    优化页面标题

--- Web/projects/CloudNote/src/router/index.js
+++ Web/projects/CloudNote/src/router/index.js
@@ -4,16 +4,19 @@ import ThingIndexView from "../views/thing/ThingIndexView.vue"
 const routes = [
     {
         path:'/memo',
-        component:ThingIndexView
+        component:ThingIndexView,
+        meta:{title:'便签页'}
     },
     {
         path:'/note',
         component:()=>import('../views/note/IndexView.vue'),
+        meta:{title:'笔记页'},
         children:[
             {
                 path:'edit/:id',
                 props:true,
-                component:()=>import('../views/note/EditNote.vue')
+                component:()=>import('../views/note/EditNote.vue'),
+                meta:{title:'笔记编辑页'}
             }
         ]
     }
@@ -22,6 +25,19 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
+})
+
+//前置守卫 路由跳转之前的处理
+router.beforeEach((to,from,next)=>{
+
+    //如果跳转路由规则中含有标题，则更改页面标签页标题
+    if(to.meta.title)
+    {
+        document.title = to.meta.title;
+    }
+
+    //跳转至指定路由
+    next();
 })
 
 export default router
