7484ddad21dc1aa78ad8e30525f8481ad7d7b809
wangwei <wangwei@smartee.local>
2024-10-11 11:38:06 +0800

    删除提醒框优化

--- Web/projects/CloudNote/src/components/remind/DeleteRemindDialog.vue
+++ Web/projects/CloudNote/src/components/remind/DeleteRemindDialog.vue
@@ -1,5 +1,6 @@
 <template>
-    <n-modal :show="show" preset="dialog" title="删除提醒" type="warning" :closable="false" transform-origin="center">
+    <n-modal :show="show" preset="dialog" title="删除提醒" 
+    type="warning" :closable="false" transform-origin="center">
         <template #icon>
             <n-icon style="position:relative;top:-2px" :component="ReportGmailerrorredRound"></n-icon>
         </template>
@@ -7,7 +8,7 @@
         <template #action>
             <n-button v-if="completeDeletBtn" size="small" tertiary type="error" @click="emits('delete',true)">彻底删除</n-button>
             <n-button v-if="deleteBtn" size="small" secondary type="error" @click="emits('delete',false)">删除</n-button>
-            <n-button size="small" tertiary @click="emits('cancel','取消')">取消</n-button>
+            <n-button size="small" tertiary @click="show=false">取消</n-button>
         </template>
     </n-modal>
 </template>
@@ -17,35 +18,44 @@
         ReportGmailerrorredRound
     } from "@vicons/material"
 
-    //自定义属性
-    const propsData = defineProps({
-        show:{type:Boolean,deafult:false},//是否显示提醒框
-        title:{type:String},//删除单个文件的标题
-        size:{type:Number,default:1},//删除文件个数
-        deleteBtn:{type:Boolean,default:true},//删除按钮是否显示
-        completeDeletBtn:{type:Boolean,default:true}//彻底删除按钮是否显示
-    })
+    import {useDeleteRemindDialogStore} from '@/stores/deleteRemindDialogStore'
+    import { storeToRefs } from "pinia";
+
+    const deleteRemindDialogStore = useDeleteRemindDialogStore();
+    const {
+        show,//是否显示提醒框
+        fileName,//删除单个文件的标题
+        size,//删除文件个数
+        deleteBtn,//删除按钮是否显示
+        completeDeletBtn,//彻底删除按钮是否显示
+        type//删除文件类型 1 笔记 2 便签
+    } = storeToRefs(deleteRemindDialogStore)
 
     //自定义事件 彻底删除 删除 取消
     const emits = defineEmits(['delete','cancel'])
 
     const description = computed(()=>{
-        if(propsData.size == 1)
+        if(size.value == 1)
         {
-            if(propsData.completeDeletBtn)
+            if(completeDeletBtn.value)
             {
-                return '确认彻底删除('+propsData.title + ')？无法恢复！';
-                
+                return '确认彻底删除('+fileName.value + ')？无法恢复！';
             }
             else
             {
-                return '确认删除('+propsData.title + ')？可在回收站中恢复。';
+                return '确认删除('+fileName.value + ')？可在回收站中恢复。';
             }
-            
         }
         else
         {
-            return '批量删除多个文件？';
+            if(completeDeletBtn.value)
+            {
+                return '确认彻底删除'+size.value + '个文件？无法恢复！';
+            }
+            else
+            {
+                return '确认删除'+size.value + '个文件？可在回收站中恢复。';
+            }
         }
     })
 </script>
---
+++ Web/projects/CloudNote/src/stores/deleteRemindDialogStore.js
@@ -0,0 +1,88 @@
+import { defineStore } from 'pinia'
+import { computed, ref,watch } from 'vue'
+
+export const deleteRemindDialogStore = defineStore(
+    "delete-remind-dialog",
+    ()=>{
+        //共享属性
+        //是否显示
+        const show = ref(false)
+        //删除的文件名
+        const fileName=ref(null)
+        //删除文件个数
+        const size=ref(1)
+        //是否显示删除按钮
+        const deleteBtn = ref(true)
+        //彻底删除按钮
+        const completeDeleteBtn = ref(true)
+        //删除的文件类型 1 笔记 2 便签
+        const type =ref(1)
+
+        /**
+         * 默认窗口
+         * @param {String} file 
+         * @param {Number} fileType 
+         */
+        const showDefault = (file,fileType)=>{
+            show.value = true;
+            fileName.value = file;
+            type.value = fileType;
+        }
+
+        /**
+         * 笔记删除提醒框
+         * @param {String} file 
+         */
+        const showFromNote = (fileId,file)=>{
+            show.value = true;
+            fileName.value = file;
+            type.value = 1;
+        }
+
+        /**
+         * 便签删除提醒框
+         * @param {String} file 
+         */
+        const showFromMemo = (fileId,file)=>{
+            show.value = true;
+            fileName.value = file;
+            type.value = 2;
+        }
+
+        /**
+         * 回收站单文件删除
+         * @param {*} file 
+         * @param {*} fileType 
+         */
+        const showFromDumpsterSingle = (fileId,file,fileType)=>{
+            show.value = true;
+            fileName.value = file;
+            type.value = fileType;
+            deleteBtn.value = false;
+        }
+
+        /**
+         * 回收站多文件删除
+         * @param {*} numOfFiles 
+         */
+        const showFromDumpsterMulti = (numOfFiles)=>{
+            size.value = numOfFiles;
+            show.value = true;
+        }
+
+        //关闭窗口，恢复初始值
+        watch(
+            ()=>show.value,
+            newData=>{
+                if(!newData){
+                    fileName.value = null;
+                    size.value = 1;
+                    deleteBtn.value = true;
+                    completeDeleteBtn.value = true;
+                    type.value =1;
+                    id.value = null;
+                }
+            })
+        return {show,fileName,size,deleteBtn,completeDeleteBtn,type}
+    }
+)
