3f22ca6da739151d3da4782ec74d65ca54de9a8a
wangwei <wangwei@smartee.local>
2024-10-16 11:58:31 +0800

    新增文件卡片组件

---
+++ Web/projects/CloudNote/src/components/card/fileCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <n-card embedded :bordered="false" size="small" hoverable>
+        <n-space align="center" :wrap-item="false">
+            <n-icon :size="32" :component="StickyNote2Outlined"></n-icon>
+            <n-space vertical :size="4">
+                <n-text>Vue3 知识点整理</n-text>
+                <n-text depth="3">4个月前</n-text>
+            </n-space>
+        </n-space>
+    </n-card>
+</template>
+
+<script setup>
+    import { EventNoteRound,StickyNote2Outlined,ShoppingBagOutlined } from '@vicons/material';
+</script>
