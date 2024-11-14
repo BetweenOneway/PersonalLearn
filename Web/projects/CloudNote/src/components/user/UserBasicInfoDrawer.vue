296aed1cab50ebb0a05b9bfa96ca96a895a5caac
wangwei <wangwei@smartee.local>
2024-11-14 11:37:53 +0800

    实现头像裁剪框基本功能

--- Web/projects/CloudNote/src/components/user/UserBasicInfoDrawer.vue
+++ Web/projects/CloudNote/src/components/user/UserBasicInfoDrawer.vue
@@ -6,7 +6,7 @@
             <n-space justify="center">
                 <label>
                     <n-avatar round :size="120" :src="head_image"></n-avatar>
-                    <input style="display:none" type="file" accept="image/jpeg,image/jpg,image/png,image/gif">
+                    <input ref="fileInputRef" style="display:none" type="file" accept="image/jpeg,image/jpg,image/png,image/gif" @change="selectImageFile">
                 </label>
             </n-space>
 
@@ -53,13 +53,13 @@
     </n-drawer>
 
     <!--头像上传窗口-->
-    <n-modal :show="true" preset="card" closable segmented title="头像上传" 
-    width="800px" height="560px" content-style="height:0">
+    <n-modal v-model:show="showCropper" preset="card" closable segmented title="头像上传" 
+    width="800px" height="560px" content-style="height:0" :mask-closable="false" :close-on-sec="false">
         <template #default>
             <n-space justify="space-between" :wrap-item="false" style="height: 100%">
                 <!--裁剪框元素-->
                 <div style="height:100%;width:calc(100%-200px)">
-                    <img id="image" src="picture.jpg" style="max-height: 100%;max-width: 100%;">
+                    <img id="image" :src="picSrc" style="max-height: 100%;max-width: 100%;">
                 </div>
                 
                 <!--裁剪预览区域-->
@@ -83,7 +83,7 @@
         
         <template #action>
             <n-space>
-                <n-button>取消</n-button>
+                <n-button @click="show=false">取消</n-button>
                 <n-button>确定</n-button>
             </n-space>
         </template>
@@ -260,9 +260,17 @@
         return ts > Date.now();
     };
 
+    //--------------图像裁剪----------------
+    let cropper = null
+
     const initCropper = ()=>{
         const image = document.getElementById('image');
-        const cropper = new Cropper(image, {
+        
+        //
+        cropper?.destroy();
+
+        //创建Cropper实例
+        cropper = new Cropper(image, {
             dragMode:'move',//拖拽模式
             viewMode:1,//视图模式
             aspectRatio:1,//长宽比
@@ -271,6 +279,38 @@
             toggleDragModeOnDblclick:false,//禁用双击模式切换
             preview:'.img-preview',//裁剪框的预览
         })
+    }
+
+    //裁剪框窗口显示状态
+    const showCropper = ref(false);
+
+    //头像图片
+    const picSrc = ref(null);
+
+    //文件选择控件
+    const fileInputRef = ref(null);
+
+    const selectImageFile = (e)=>{
+        //选中图像的重置
+        picSrc.value = null;
+        const {files} = e.target;
+        if(!!files || !files.length) return;
+        const file = files[0];
+        const reader = new FileReader();
+        //读取文件 base64
+        reader.readAsDataURL(file);
+        reader.onload = ()=>{
+            //显示图像文件
+            picSrc.value = String(reader.result)
+            //显示头像上传窗口
+            showCropper.value = true;
+            //清除文件选择控件的值
+            if(fileInputRef.value) fileInputRef.value.value = ''
+            //视图更新完成后 初始化裁剪框
+            nextTick(()=>{
+                initCropper()
+            })
+        }
     }
 
     defineExpose({changeActive})
