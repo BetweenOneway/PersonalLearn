ae750aedaa864b7988b472b31bfe94e9a28f4b5e
wang wei <wangwei@desktop.com>
2024-11-26 23:44:54 +0800

    新增学习Element Plus项目

---
+++ Web/LearnElementPlus/vueElement/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </div>
+</template>
+
+<style scoped>
+.el-container{
+    height: 100vh;
+}
+.el-header{
+    background-color: aqua;
+}
+.el-main{
+    background-color: burlywood;
+}
+.el-aside{
+    background-color: cornflowerblue;
+}
+</style>
