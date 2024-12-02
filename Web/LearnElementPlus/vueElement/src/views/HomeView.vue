0d70a54b23206de3206d319678db8a3295f2aaa4
wangwei <wangwei@smartee.local>
2024-12-02 13:19:28 +0800

    优化界面样式

--- Web/LearnElementPlus/vueElement/src/views/HomeView.vue
+++ Web/LearnElementPlus/vueElement/src/views/HomeView.vue
@@ -3,8 +3,8 @@
         <el-row align="middle">
             <el-col :span="12">
                 <h1 class="caption">云笔记</h1>
-                <p>任何时间 任何地点 任何设备 一触即达</p>
-                <el-button type="primary" size="large">开始使用</el-button>
+                <p class="desc mb-1">任何时间 任何地点 任何设备 一触即达</p>
+                <el-button type="primary" size="large" class="mt-3">开始使用</el-button>
             </el-col>
             <el-col :span="12">
                 <el-image :src="illustration3"></el-image>
@@ -25,9 +25,19 @@
 .pb-6 {
     padding-bottom:6rem;
 }
+.mb-1{
+    margin-bottom:1rem;
+}
+.mt-3{
+    margin-top:3rem;
+}
 .caption{
     font-size:80px;
     font-weight: 900;
     line-height: 1;
+}
+.desc{
+    font-size:1.125rem;
+    font-weight: 300;
 }
 </style>
