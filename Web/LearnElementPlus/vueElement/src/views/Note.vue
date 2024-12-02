c6233860fc37d873b0ceabfc6920ae83d9b56c45
wang wei <wangwei@desktop.com>
2024-12-02 23:46:26 +0800

    修改笔记界面布局

--- Web/LearnElementPlus/vueElement/src/views/Note.vue
+++ Web/LearnElementPlus/vueElement/src/views/Note.vue
@@ -1,9 +1,34 @@
 <template>
+    <div>
+        <el-container>
+            <el-aside width="208px">
+                <el-space class="sidebar">
+                    Avatar
+                </el-space>
+            </el-aside>
+            <el-container>
+                <el-aside width="312px">笔记本内容区域</el-aside>
+                <el-container>
+                    <el-header>标题栏</el-header>
+                    <el-main>编辑器区域</el-main>
+                </el-container>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
+@import url('@/assets/reset.css');
 @import url('@/assets/main.css');
+.sidebar{
+    padding-top: 32px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    border: 1px solid black;
+    box-sizing: border-box;
+}
 </style>
