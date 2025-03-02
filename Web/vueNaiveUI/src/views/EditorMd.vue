038c5ee2fd9c923a5e1fcae07aa1a30158d865d2
betweenoneway <oneway2021@163.com>
2025-03-02 15:23:19 +0800

    实现EditorMdV3的编辑与预览模式切换

--- Web/vueNaiveUI/src/views/EditorMd.vue
+++ Web/vueNaiveUI/src/views/EditorMd.vue
@@ -1,19 +1,29 @@
 <template>
-    <MdEditor v-model="text" :toolbarsExclude="toolbarsExclude" 
-    @onUploadImg="onUploadImg" 
-    @onSave="codeSave"/>
+    <n-button @click="modeChange">预览/编辑模式切换</n-button>
+    <n-space v-if="isPreviewMode">
+        <MdPreview :editorId="id" :modelValue="text" />
+        <MdCatalog :editorId="id" :scrollElement="scrollElement" />
+    </n-space>
+    <n-space v-else>
+        <MdEditor v-model="text" :toolbarsExclude="toolbarsExclude" 
+        @onUploadImg="onUploadImg" 
+        @onSave="codeSave"/>
+    </n-space>
+    
 </template>
   
 <script setup>
   import { ref } from 'vue';
-  import { MdEditor } from 'md-editor-v3';
+  import { MdEditor,MdPreview, MdCatalog } from 'md-editor-v3';
   import 'md-editor-v3/lib/style.css';
   
-  const text = ref('Hello Editor!');
+    const text = ref('Hello Editor!');
+    let isPreviewMode = ref(false)
 
-  const toolbars = ['italic', 'underline', '-', 'bold', '=', 'github'];
-  const toolbarsExclude=['link', 'mermaid', 'katex', 'github'];
-
+    const toolbars = ['italic', 'underline', '-', 'bold', '=', 'github'];
+    const toolbarsExclude=['link', 'mermaid', 'katex', 'github'];
+    const scrollElement = document.documentElement;
+    
     function onUploadImg(files, callback)
     {
         console.log("uploadImg=>",files);
@@ -22,5 +32,9 @@
     function codeSave(v)
     {
         console.log("save=>",v);
+    }
+    function modeChange()
+    {
+        isPreviewMode.value = !isPreviewMode.value
     }
 </script>
