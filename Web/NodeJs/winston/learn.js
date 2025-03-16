a956b8147d8011ab7e383c83a027d96041ab4f5d
wangwei <wangwei@smartee.local>
2025-03-16 16:21:13 +0800

    winston学习新增用法测试

--- Web/NodeJs/winston/learn.js
+++ Web/NodeJs/winston/learn.js
@@ -3,3 +3,5 @@ let winston = require('./log.js');
 winston.log('info', 'Hello log!')
 winston.info('Hello info')
 winston.error('Hello error')
+let name="wangwei";
+winston.error(`Hello error ${name}`)
