23e2fb85b01707cafc9cd83de91c26c4ff89e550
wangwei <wangwei@smartee.local>
2025-11-06 11:26:31 +0800

    丰富协程案例

--- PythonLearn/processThread/coroutine.py
+++ PythonLearn/processThread/coroutine.py
@@ -1,7 +1,11 @@
 from gevent import monkey
+import gevent.pool
 monkey.patch_all()
 import urllib.request
 from gevent.pool import Pool
+import gevent
+
+global urls
 
 def run_task(url):
     print('visit -->%s'%url)
@@ -13,8 +17,20 @@ def run_task(url):
         print(e)
     return 'url:%s-->finish'%url
 
-if __name__=='__main__':
+# 协程池的方式
+def runWithPool():
     pool = Pool(2)
-    urls=['https://cn.bing.com/','https://www.python.org','https://www.cnblogs.com/']
+    # urls=['https://cn.bing.com/','https://www.python.org','https://www.cnblogs.com/']
     results = pool.map(run_task,urls)
     print(results)
+
+# 直接使用协程方式
+def simpleCoroutine():
+    # urls=['https://cn.bing.com/','https://www.python.org','https://www.cnblogs.com/']
+    greenlets = [gevent.spawn(run_task,url) for url in urls]
+    gevent.joinall(greenlets)
+
+if __name__=='__main__':
+    urls=['https://cn.bing.com/','https://www.python.org','https://www.cnblogs.com/']
+    # runWithPool()
+    simpleCoroutine()
