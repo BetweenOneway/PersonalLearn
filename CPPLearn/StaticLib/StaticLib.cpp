e74fe579e0b8681ab4c37b1c233ddab2abb2ad91
wangwei <wangwei@smartee.local>
2025-11-18 17:51:18 +0800

    新增静态库测试程序

---
+++ CPPLearn/StaticLib/StaticLib.cpp
@@ -0,0 +1,12 @@
+﻿// StaticLib.cpp : 定义静态库的函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <iostream>
+
+// TODO: 这是一个库函数示例
+void fnStaticLib(int i)
+{
+    std::cout << "fnStaticLib-->" << i << std::endl;
+}
