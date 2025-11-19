0b93fb36a6a133f5712abe4c1a797702f648b4e8
wangwei <wangwei@smartee.local>
2025-11-19 13:15:03 +0800

    新增静态库调用静态库项目
    静态库项目 移除预处理文件 重命名文件名

--- CPPLearn/StaticLib/StaticLib.cpp
+++ CPPLearn/StaticLib/StaticLib.cpp
@@ -1,8 +1,7 @@
 ﻿// StaticLib.cpp : 定义静态库的函数。
 //
 
-#include "pch.h"
-#include "framework.h"
+#include "StaticLib.h"
 #include <iostream>
 
 // TODO: 这是一个库函数示例
---
+++ CPPLearn/StaticLibCallStaitcLib/StaticLibCallStaitcLib.cpp
@@ -0,0 +1,10 @@
+﻿// StaticLibCallStaitcLib.cpp : 定义静态库的函数。
+//
+
+#include "StaticLibCallStaticLib.h"
+#include <iostream>
+// TODO: 这是一个库函数示例
+void fnStaticLibCallStaitcLib()
+{
+    std::cout << "fnStaticLibCallStaticLib"<<std::endl;
+}
