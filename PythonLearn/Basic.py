0b4313c7dda1184be8fd32325b632c949b1a26a9
wangwei <wangwei@smartee.local>
2025-12-10 11:39:12 +0800

    学习python格式化字符串

--- PythonLearn/Basic.py
+++ PythonLearn/Basic.py
@@ -1,5 +1,22 @@
-requested_toppings = []
-if requested_toppings:
-    print("not empty list")
-else:
-    print("empty list")
+def testArray():
+    requested_toppings = []
+    if requested_toppings:
+        print("not empty list")
+    else:
+        print("empty list")
+
+def testFormatString():
+    name ="Alice"
+    age=25
+    print("Hello, %s. You are %d years old." % (name, age))
+    
+    print("Hello format-1, {}. You are {} years old.".format(name, age))
+
+    print(f"Hello format-2, {name}. You are {age} years old.")
+
+    template = "{name} is {age} years old."
+    
+    print(template.format(name=name, age=age))
+
+if __name__=="__main__":
+    testFormatString()
