5fe26a039f674c3f13ce3c908168fc7a8c8ef9f9
wangwei <wangwei@smartee.local>
2025-12-23 11:46:28 +0800

    新增Coroutine学习代码

--- PythonLearn/ScrapeCenter/Coroutine.py
+++ PythonLearn/ScrapeCenter/Coroutine.py
@@ -1,9 +1,11 @@
 import asyncio
+import requests
 
 async def execute(x):
     print('Number:',x)
 
 def Impl1():
+    # 这里调用async方法并未执行 而是返回一个coroutine对象
     coroutine=execute(1)
     print('Coroutine:',coroutine)
     print('after calling execute')
@@ -14,6 +16,7 @@ def Impl1():
     '''
 
     loop = asyncio.get_event_loop()
+    # 这里会隐式进行一个操作将coroutine对象封装成task对象
     loop.run_until_complete(coroutine)
     print('After calling loop')
 
@@ -24,6 +27,7 @@ def Impl2():
     print('after calling execute')
 
     loop = asyncio.get_event_loop()
+    # 显式创建task对象 将coroutine对象转换成task对象
     task = loop.create_task(coroutine)
     print("Task:",task)
     
@@ -31,5 +35,36 @@ def Impl2():
     print("Task:",task)
     print("After calling loop")
 
+async def request():
+    url="https://www.baidu.com"
+    status = requests.get(url)
+    return status
+
+def callback(task):
+    print("Status:",task.result())
+
+# 测试绑定回调
+def Impl3():
+    coroutine = request()
+    task = asyncio.ensure_future(coroutine)
+    task.add_done_callback(callback)
+    print("Task:",task)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(task)
+    print("Task:",task)
+    print("Task Result:",task.result())
+
+#多任务协程
+def Impl4():
+    tasks = [asyncio.ensure_future(request()) for _ in range(5)]
+    print("Tasks:",tasks)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.wait(tasks))
+
+    for task in tasks:
+        print("Task Result:",task.result())
+
 if __name__=="__main__":
-    Impl2()
+    Impl4()
