608cce2a8a0631b4938bd522b17abdd58cc3f362
wangwei <wangwei@smartee.local>
2026-01-14 11:06:47 +0800

    修复pyppeteer禁止弹框失效问题

--- PythonLearn/ScrapeCenter/LearnPyppeteer.py
+++ PythonLearn/ScrapeCenter/LearnPyppeteer.py
@@ -13,8 +13,33 @@ async def asyncPyppeteerUsage1():
     print('Names:',names)
     await browser.close()
 
-def PyppeteerUsage1():
-    asyncio.get_event_loop().run_until_complete(asyncPyppeteerUsage1())
+async def PyppeteerHeadless():
+    await launch(headless=False)
+    await asyncio.sleep(100)
+
+async def PyppeteerWebDriver():
+    # 原来的args=['--disable-infobars']失效了 用下面这个方法替代
+    browser = await launch(
+        {
+            'headless':False,
+            'ignoreDefaultArgs': ['--enable-automation']
+        })
+    page = await browser.newPage()
+    # 设置窗口大小
+    width,height = 1366,768
+    await page.setViewport({
+        'width':width,
+        'height':height
+    })
+    # 绕过webdriver检测
+    await page.evaluateOnNewDocument('Object.defineProperty(navigator,"webdriver",{get:()=>undefined})')
+    await page.goto('https://antispider1.scrape.center')
+    await asyncio.sleep(100)
+
+def PyppeteerUsage():
+    # asyncio.get_event_loop().run_until_complete(asyncPyppeteerUsage1())
+    # asyncio.get_event_loop().run_until_complete(PyppeteerHeadless())
+    asyncio.get_event_loop().run_until_complete(PyppeteerWebDriver())
 
 if __name__=="__main__":
-    PyppeteerUsage1()
+    PyppeteerUsage()
