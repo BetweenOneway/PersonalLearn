5fb74b32a66423335265f8545261f440831a7465
wangwei <wangwei@smartee.local>
2026-01-20 11:23:10 +0800

    充实字体反扒

--- PythonLearn/ScrapeCenter/ScrapeAntiSpider3.py
+++ PythonLearn/ScrapeCenter/ScrapeAntiSpider3.py
@@ -3,6 +3,27 @@ from pyquery import PyQuery as pq
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+import re
+
+def parse_name(name_html):
+    #print("input name_html=>",name_html)
+    has_whole = name_html('.whole')
+    if has_whole:
+        return name_html.text()
+    else:
+        chars = name_html('.char')
+        #print("type(chars)=>",type(chars))
+        #print("chars=>",chars)
+        items = []
+        for char in chars.items():
+            items.append(
+                {
+                    'text':char.text().strip(),
+                    'left':int(re.search(r'(\d+)px',char.attr('style')).group(1))
+                }
+            )
+        items = sorted(items,key=lambda x:x['left'],reverse=False)
+        return ''.join([item.get('text') for item in items])
 
 browser = webdriver.Chrome()
 browser.get('https://antispider3.scrape.center')
@@ -10,6 +31,7 @@ WebDriverWait(browser,10).until(EC.presence_of_all_elements_located((By.CSS_SELE
 html = browser.page_source
 doc = pq(html)
 names = doc('.item .name')
-for name in names.items():
-    print(name.text())
+for name_html in names.items():
+    name = parse_name(name_html)
+    print(name)
 browser.close()
