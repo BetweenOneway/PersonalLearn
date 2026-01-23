1b3acbf9253fa4e6242e8997a6004f8dc762cc68
wangwei <wangwei@smartee.local>
2026-01-23 13:10:31 +0800

    验证码OCR识别新增方法

--- PythonLearn/ScrapeCenter/captcha.py
+++ PythonLearn/ScrapeCenter/captcha.py
@@ -1,6 +1,17 @@
 import tesserocr
 from PIL import Image
+import numpy as np
 
-image = Image.open('captcha.png')
-result = tesserocr.image_to_text(image)
-print(result)
+def Captcha1():
+    image = Image.open('captcha.png')
+    result = tesserocr.image_to_text(image)
+    print(result)
+    print(tesserocr.file_to_text('captcha.png'))
+
+def Captcha2():
+    image = Image.open('captcha2.png')
+    print(np.array(image).shape)
+    print(image.mode)
+
+if __name__=='__main__':
+    Captcha2()
