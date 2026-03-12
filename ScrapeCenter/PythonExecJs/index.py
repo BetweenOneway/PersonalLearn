c2d21c96fc410a3271698237a0f36cc2f96be8ff
wangwei <wangwei@smartee.local>
2026-03-12 13:24:17 +0800

    演示python执行JS

--- ScrapeCenter/PythonExecJs/index.py
+++ ScrapeCenter/PythonExecJs/index.py
@@ -1,3 +1,23 @@
 import execjs
+import json
 
-print(execjs.get().name)
+#print(execjs.get().name)
+
+item = {
+    'name':'凯文.杜兰特',
+    'image':'durant.png',
+    'birthday':'1988-09-29',
+    'height':'208cm',
+    'weight':'108.9KG',
+}
+
+file = 'crypto.js'
+#获取js执行环境
+node = execjs.get()
+#上下文对象
+ctx = node.compile(open(file).read())
+
+js = f"getToken({json.dumps(item,ensure_ascii=False)})"
+print(js)
+result = ctx.eval(js)
+print(result)
