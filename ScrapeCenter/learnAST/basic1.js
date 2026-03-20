4421992744452772c2e1204bb0947e085a8c7590
wangwei <wangwei@smartee.local>
2026-03-20 11:42:45 +0800

    新增AST->JS 实现

--- ScrapeCenter/learnAST/basic1.js
+++ ScrapeCenter/learnAST/basic1.js
@@ -1,6 +1,13 @@
+//当前目录执行npx babel-node basic1.js
 import {parse} from "@babel/parser"
+import generate from "@babel/generator"
 import fs from "fs"
 
 const code = fs.readFileSync("codes/code1.js","utf-8")
+//JS代码=>AST
 let ast = parse(code)
 console.log(ast)
+
+//AST=>JS代码
+const {code:output} = generate(ast)
+console.log(output)
