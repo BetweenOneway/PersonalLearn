f49256ed4e13c5b291860282d6f3ecc874830c5e
wangwei <wangwei@smartee.local>
2026-03-24 10:10:57 +0800

    新增Deobfuscate文件夹

---
+++ ScrapeCenter/Deobfuscate/deobfuscate1.js
@@ -0,0 +1,19 @@
+import traverse from "@babel/traverse";
+import {parse} from "@babel/parser";
+import generate from "@babel/generator";
+import * as types from "@babel/types";
+import fs from "fs";
+
+const code = fs.readFileSync("code1.js","utf-8");
+let ast = parse(code)
+
+traverse(ast,{
+    "UnaryExpression|BinaryExpression|conditionalExpression|callExpression":(path)=>{
+        const {confident,value} = path.evaluate();
+        if(value == Infinity || value == -Infinity) return;
+        confident && path.replaceWith(types.valueToNode(value));
+    },
+})
+
+const { code:output} = generate(ast)
+console.log(output);
