1fcdd7a2eb189a9d8650d9b031daff9a4f859117
wangwei <wangwei@smartee.local>
2026-04-30 18:15:33 +0800

    测试SpiderMiddleWare

--- ScrapeCenter/Scrapy/scrapySpiderMiddlewareDemo/scrapySpiderMiddlewareDemo/spiders/httpbin.py
+++ ScrapeCenter/Scrapy/scrapySpiderMiddlewareDemo/scrapySpiderMiddlewareDemo/spiders/httpbin.py
@@ -1,10 +1,15 @@
 import scrapy
-
+from scrapy import Spider,Request
 
 class HttpbinSpider(scrapy.Spider):
     name = "httpbin"
     allowed_domains = ["www.httpbin.org"]
-    start_urls = ["https://www.httpbin.org"]
+    start_url = "https://www.httpbin.org/get"
 
     def parse(self, response):
-        pass
+        print(response.text)
+
+    def start_requests(self):
+        for i in range(5):
+            url = f'{self.start_url}?query={i}'
+            yield Request(url,callback=self.parse)
