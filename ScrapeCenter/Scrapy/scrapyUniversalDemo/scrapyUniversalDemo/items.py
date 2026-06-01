3e4bd461a04d3b7df28002afe6038aed55ac3d88
wangwei <wangwei@smartee.local>
2026-06-01 13:21:48 +0800

    充实Scrapy规则化爬虫实现

--- ScrapeCenter/Scrapy/scrapyUniversalDemo/scrapyUniversalDemo/items.py
+++ ScrapeCenter/Scrapy/scrapyUniversalDemo/scrapyUniversalDemo/items.py
@@ -10,3 +10,13 @@ class ScrapyuniversaldemoItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+from scrapy import Field,Item
+
+class MovieItem(Item):
+    name = Field()
+    cover = Field()
+    categories = Field()
+    published_at =Field()
+    drama = Field()
+    score = Field()
--- ScrapeCenter/Scrapy/scrapyUniversalDemo/scrapyUniversalDemo/spiders/movie.py
+++ ScrapeCenter/Scrapy/scrapyUniversalDemo/scrapyUniversalDemo/spiders/movie.py
@@ -2,15 +2,19 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+from scrapyUniversalDemo.items import MovieItem
 
 class MovieSpider(CrawlSpider):
     name = "movie"
     allowed_domains = ["ssr1.scrape.center"]
     start_urls = ["https://ssr1.scrape.center"]
 
-    rules = (Rule(LinkExtractor(restrict_css='.item .name'), callback="parse_detail", follow=True),)
+    rules = (
+        Rule(LinkExtractor(restrict_css='.item .name'), callback="parse_detail", follow=True),
+        Rule(LinkExtractor(restrict_css='.next'),follow=True),)
 
     def parse_item(self, response):
+        print('call parse_item')
         item = {}
         #item["domain_id"] = response.xpath('//input[@id="sid"]/@value').get()
         #item["name"] = response.xpath('//div[@id="name"]').get()
@@ -19,3 +23,11 @@ class MovieSpider(CrawlSpider):
     
     def parse_detail(self,response):
         print(response.url)
+        item = MovieItem()
+        item['name'] = response.css('.item h2::text').extract_first()
+        item['categories'] = response.css('.categories button span::text').extract_first()
+        item['cover'] = response.css('.cover::attr(src)').extract_first()
+        item['published_at'] = response.css('.info span::text').re_first('(\d{4}-\d{2}-\d{2})\s?上映')
+        item['score'] = response.xpath('//p[contains(@class,"score")]/text()').extract_first().strip()
+        item['drama'] = response.xpath('//div[contains(@class,"drama")]/p/text()').extract_first().strip()
+        yield item
