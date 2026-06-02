9d7b3fe45a5d000db2fe6af8d47a8dbd305d2c71
wangwei <wangwei@smartee.local>
2026-06-02 13:14:47 +0800

    规则化scrapyUniversalDemo

--- ScrapeCenter/Scrapy/scrapyUniversalDemo/scrapyUniversalDemo/spiders/universal.py
+++ ScrapeCenter/Scrapy/scrapyUniversalDemo/scrapyUniversalDemo/spiders/universal.py
@@ -1,7 +1,8 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-
+from ..utils import get_config
+from .. import items, loaders
 
 class UniversalSpider(CrawlSpider):
     name = "universal"
@@ -9,6 +10,37 @@ class UniversalSpider(CrawlSpider):
     start_urls = ["https://universal"]
 
     rules = (Rule(LinkExtractor(allow=r"Items/"), callback="parse_item", follow=True),)
+
+    def __init__(self,name,*args,**kwargs):
+        config = get_config(name)
+        self.config = config
+        self.start_urls = config.get('start_urls')
+        self.allowed_domains = config.get('allowes_domains')
+        rules = []
+        for rule_kwargs in config.get('rules'):
+            link_extractor = LinkExtractor(**rule_kwargs.get('link_extractor'))
+            rule_kwargs['link_extractor'] = link_extractor
+            rule = Rule(**rule_kwargs)
+            rules.append(rule)
+        self.rules = rules
+        super(UniversalSpider,self).__init__(*args,**kwargs)
+
+    def parse_detail(self,response):
+        item = self.config.get('item')
+        if item:
+            cls = getattr(items,item.get('class'))()
+            loader = getattr(loaders,item.get('loader'))(cls,response=response)
+            for key,value in item.get('attrs').items():
+                for extractor in value:
+                    method = extractor.get('method')
+                    if method =='xpath':
+                        loader.add_xpath(key,extractor.get('arg'),**{'re':extractor.get('re')})
+                    if method == 'css':
+                        loader.add_css(key,extractor.get('arg'),**{'re':extractor.get('re')})
+                    if method == 'value':
+                        loader.add_value(key,extractor.get('args'),**{'re':extractor.get('re')})
+            yield loader.load_item()
+
 
     def parse_item(self, response):
         item = {}
