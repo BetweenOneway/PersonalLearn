980129314e5691a661ca603423c2a2674483afa9
wangwei <wangwei@smartee.local>
2026-06-16 13:04:27 +0800

    移除重复项目

--- between-network/ScrapeCenter/nes_spider/nes_spider/items.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-class NesSpiderItem(scrapy.Item):
-    title = scrapy.Field()
-    url = scrapy.Field()
-    publish_time = scrapy.Field()
-    author = scrapy.Field()
-    content = scrapy.Field()
-    category = scrapy.Field()
--- between-network/ScrapeCenter/nes_spider/nes_spider/pipelines.py
+++
@@ -1,3 +0,0 @@
-class NesSpiderPipeline:
-    def process_item(self, item, spider):
-        return item
