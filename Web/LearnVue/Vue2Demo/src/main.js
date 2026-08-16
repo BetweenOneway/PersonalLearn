6897b8515b7a71387f1513dc465f36f2016ed7a0
betweenoneway <oneway2021@163.com>
2026-08-16 21:34:55 +0800

    新增Vue2学习Demo

---
+++ Web/LearnVue/Vue2Demo/src/main.js
@@ -0,0 +1,25 @@
+//启动代码
+import Vue from '../lib/vue.js'
+import App from './App.js'
+
+new Vue({
+    el:'#app',
+    data:{
+        title:'My First Vue',
+    },
+    //局部注册
+    components:{
+        'app':App
+    },
+    template:`<app></app>`,
+    render(h){
+        //效果和template一样,这个方式连组件都不需要注册
+        return h(App)
+    }
+})
+
+//Vue挂在的第二种方式，与el效果相同
+/*
+const vm = new Vue()
+vue.$mount('#app')
+*/
